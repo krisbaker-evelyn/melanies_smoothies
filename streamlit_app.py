18ff06ca8eca9cc2892ff6994a010762952f0ae2
krisbaker-evelyn <kris.baker@evelyn.com>
2026-05-18 15:29:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
-from snowflake.snowpark.functions import col
-
 # Import python packages.
 import streamlit as st
-
+from snowflake.snowpark.functions import col
 
 # Write directly to the app.
 st.title(f":cup_with_straw: Customise Your Smoothie:cup_with_straw: ")
