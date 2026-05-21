b987327b20d759189fa1c75399630e0b68ded0d9
krisbaker-evelyn <kris.baker@evelyn.com>
2026-05-21 16:04:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,12 @@ warehouse="COMPUTE_WH"
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop
+
+
+
 name_on_order = st.text_input('Name on Smoothie:')
 
 if name_on_order:
