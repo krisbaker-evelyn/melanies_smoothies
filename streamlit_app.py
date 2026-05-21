448b98e1c0744b7a5db66daef327127a89c77255
krisbaker-evelyn <kris.baker@evelyn.com>
2026-05-21 16:01:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,10 +27,6 @@ if name_on_order:
 ingredients_list=st.multiselect('Choose up to 5 ingredients:' , my_dataframe
                                , max_selections=5)
 
-search_list=st.multiselect( my_dataframe2
-                               , max_selections=5)
-
-
 if ingredients_list:
     #st.write(ingredients_list)
     #st.text(ingredients_list)
